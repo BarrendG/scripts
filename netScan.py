a3753a35a19ee5e3f925367cd23b0d657b68d591
pilias <pilias@it.auth.gr>
2018-12-05 15:18:40 +0200

    completed network scanner

--- netScan.py
+++ netScan.py
@@ -2,10 +2,13 @@ import optparse
 import socket
 from socket import *
 def connScan(tgtHost, tgtPort):
-	try
+	try:
 		connSkt = socket(AF_INET, SOCK_STREAM)
-		connSkt.connect((tgtHost, tgtPort))
+	 	connSkt.connect((tgtHost, tgtPort))
+		connSkt.send('ViolentPython\r\n')
+		results = connSkt.recv(100)
 		print '[+]%d/tcp open'% tgtPort
+		print '[+] ' + str(results)
 		connSkt.close()
 	except:
 		print '[-]%d/tcp closed'% tgtPort
@@ -24,13 +27,16 @@ def portScan(tgtHost,tgtPorts):
 		print 'Scanning port ' + tgtPort
 		connScan(tgtHost, int(tgtPort))
 
-
-parser = optparse.OptionParser('usage %prog -H'+'<target host> -p <target port>')
-parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
-parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
-(options, args) = parser.parse_args()
-tgtHost = options.tgtHost
-tgtPort = options.tgtPort
-if (tgtHost == None) | (tgtPort == None):
-	print parser.usage
-	exit(0)
+def main():
+	parser = optparse.OptionParser('usage %prog -H'+'<target host> -p <target port>')
+	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+	parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+	(options, args) = parser.parse_args()
+	tgtHost = options.tgtHost
+	tgtPort = options.tgtPort
+	if (tgtHost == None) | (tgtPort == None):
+		print parser.usage
+		exit(0)
+	portScan(tgtHost, tgtPorts)
+if __name__=='__main__':
+	main()
