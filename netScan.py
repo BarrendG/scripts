209f2da8aa6fff1db9acdca30964c22422408d76
pilias <pilias@it.auth.gr>
2018-12-05 15:35:14 +0200

    network scanner added threading

--- netScan.py
+++ netScan.py
@@ -1,17 +1,24 @@
 import optparse
 import socket
 from socket import *
+from threading import *
+screenLock = Semaphore(value=1)
 def connScan(tgtHost, tgtPort):
 	try:
 		connSkt = socket(AF_INET, SOCK_STREAM)
 	 	connSkt.connect((tgtHost, tgtPort))
 		connSkt.send('ViolentPython\r\n')
 		results = connSkt.recv(100)
+		screenLock.acquire()
 		print '[+]%d/tcp open'% tgtPort
 		print '[+] ' + str(results)
 		connSkt.close()
 	except:
+		screenLock.acquire()
 		print '[-]%d/tcp closed'% tgtPort
+	finally:
+		screenLock.release()
+		connSkt.close()
 def portScan(tgtHost,tgtPorts):
 	try:
 		tgtIP = gethostbyname(tgtHost)
@@ -20,20 +27,20 @@ def portScan(tgtHost,tgtPorts):
 		return
 	try:
 		tgtName = gethostbyaddr(tgtIP)
+		print '\n[+] Scan Results for: '+tgtName[0]
 	except:
-		print '\n[+] Scan Results for: ' + tgtName[0]
+		print '\n[+] Scan Results for: ' + tgtIP
 	setdefaulttimeout(1)
 	for tgtPort in tgtPorts:
-		print 'Scanning port ' + tgtPort
-		connScan(tgtHost, int(tgtPort))
-
+		t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+		t.start()
 def main():
-	parser = optparse.OptionParser('usage %prog -H'+'<target host> -p <target port>')
+	parser = optparse.OptionParser('usage %prog -H '+'<target host> -p <target port>')
 	parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
 	parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
 	(options, args) = parser.parse_args()
 	tgtHost = options.tgtHost
-	tgtPort = options.tgtPort
+	tgtPort = str(options.tgtPort).split('. ')
 	if (tgtHost == None) | (tgtPort == None):
 		print parser.usage
 		exit(0)
