191a662df1977f0022803a7b9ea75578cac1a851
pilias <pilias@it.auth.gr>
2018-12-05 09:59:49 +0200

    Crackers corrected

--- UnixPassCrack.py
+++ UnixPassCrack.py
@@ -3,7 +3,7 @@ def testPass(cryptPass):
 	salt=cryptPass[0:2]
 	dictFile = open ('dictionary.txt','r')
 	for word in dictFile.readlines():
-		wod = word.strip('\n')
+		word = word.strip('\n')
 		cryptWord = crypt.crypt(word,salt)
 		if (cryptWord == cryptPass):
 			print "[+] Found Password: "+word+"\n"
@@ -15,7 +15,7 @@ def main():
 	for line in passFile.readlines():
 		if ":" in line:
 			user = line.split(':')[0]
-			cryptPass = line.split(':')[1](' ')
+			cryptPass = line.split(':')[1]strip.(' ')
 			print "[*] Cracking Password For: "+user
 			testPass(cryptPass)
 if __name__=="__main__":
--- UnixPassCrackSHA512.py
+++ UnixPassCrackSHA512.py
@@ -1,9 +1,12 @@
-import hashlib
+import crypt
 def testPass(cryptPass):
+	ctype= "6"
+	salt="Ug1ZA3.A"
+	insalt = '${}${}$'.format(ctype,salt)
 	dictFile = open ('dictionary.txt','r')
 	for word in dictFile.readlines():
 		word = word.strip('\n')
-		cryptWord = hashlib.sha512(word).hexdigest()
+		cryptWord = crypt.crypt(word,insalt)
 		if (cryptWord == cryptPass):
 			print "[+] Found Password: "+word+"\n"
 			return
@@ -14,7 +17,7 @@ def main():
 	for line in passFile.readlines():
 		if ":" in line:
 			user = line.split(':')[0]
-			cryptPass = line.split(':')[1].strip(' ')
+			cryptPass = line.split(':')[1]
 			print "[*] Cracking Password For: "+user
 			testPass(cryptPass)
 if __name__=="__main__":
