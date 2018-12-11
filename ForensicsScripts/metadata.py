5d908e1deea4f8f21f7fb7c651038abf97211d5a
pilias <pilias@it.auth.gr>
2018-12-11 14:32:38 +0200

    Adding Forensics RecycleBin & Metadata

---
+++ ForensicsScripts/metadata.py
@@ -0,0 +1,20 @@
+import PyPDF2
+import optparse
+from PyPDF2 import PdfFileReader
+def printMeta(fileName):
+	pdfFile = PdfFileReader(file(fileName, 'rb'))
+	docInfo = pdfFile.getDocumentInfo()
+	print '[*] PDF MetaData For: ' + str(fileName)
+	for metaItem in docInfo
+		print '[+] ' + metaItem + ':' + docInfo[metaItem]
+def main():
+	parser = optparse.OptionParser('usage%prog " + "-F <PDF file name>')
+	parser.add_option('-F', dest='fileName', type='string', help='specify PFG file name')
+	(options, args) = parser.parse_args()
+	fileName = options.fileName
+	if fileName == None:
+		print parser.usage
+		exit(0)
+	else:
+if __name__=='__main__':
+	main()
